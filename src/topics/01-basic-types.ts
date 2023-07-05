137a11d4472a66efb51a55604b2da83383a71284
PabloBeRui <pablo.bellon.ruibal@gmail.com>
2023-07-05 13:47:19 +0200

    First contact with Vite

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,12 @@
+const name: string = "Pablo";
+
+let hpPoints: number | string = 95;
+
+const isAlive: boolean = true;
+
+hpPoints = "hola mundo";
+hpPoints = 95;
+
+console.log(name, hpPoints, isAlive);
+
+export {};
