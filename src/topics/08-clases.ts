d2f978642c858c894a6c27971ff3b1ee98f71574
PabloBeRui <pablo.bellon.ruibal@gmail.com>
2023-07-18 19:18:29 +0200

    add prioritize composition

--- src/topics/08-clases.ts
+++ src/topics/08-clases.ts
@@ -8,16 +8,34 @@ export class Person {
   }
 }
 
-export class Hero extends Person {
-  constructor(
-    public alterEgo: string,
-    public age: number,
-    public realName: string
-  ) {
-    super(realName,'Betanzos');
-  }
-}
+// export class Hero extends Person {
+//   constructor(
+//     public alterEgo: string,
+//     public age: number,
+//     public realName: string
+//   ) {
+//     super(realName,'Betanzos');
+//   }
+// }
 
-const ironman = new Hero("Ironman", 43,'tony');
+export class Hero  {
+
+// public person:Person
+
+    constructor(
+      public alterEgo: string,
+      public age: number,
+        public realName: string,
+        public person:Person
+    ) {
+    //  this.person=new Person(realName)
+    }
+}
+  
+
+const tony=new Person ('Tony Stark','New York')
+
+// const ironman = new Hero("Ironman", 43,'tony');
+const ironman = new Hero("Ironman", 43,'tony',tony)
 
 console.log(ironman);
